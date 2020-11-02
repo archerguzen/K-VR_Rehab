bf3e904922898b5851dc0f82ffbb544c35edfeea
archerguzen <a.eguzen@gmail.com>
2020-11-02 23:18:43 +0300

    Some of the codes editted.

--- K-VR Unity Project/Assets/Scripts/UI_Scripts/SelectionRoomMenu.cs
+++ K-VR Unity Project/Assets/Scripts/UI_Scripts/SelectionRoomMenu.cs
@@ -2,19 +2,76 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class SelectionRoomMenu : MonoBehaviour
 {
     private string ballType;
+    private string[] footballMovements;
+    private string[] volleyballMovements;
+    private string[] basketballMovements;
+
+    private VideoPlayer videoPlayer;
+
+    public void Awake()
+    {
+        footballMovements = new string[] { "Yön Değiştirmeli Dribling", "Engelli Pas Çalışması" };
+        volleyballMovements = new string[] { "Pas Manşet", "Smaç Sıçraması" };
+        basketballMovements = new string[] { "Bacak Arası Crossover Sıçraması", "Top Sürme ve Durma" };
+    }
+
+    public void Update()
+    {
+        
+    }
 
     public void setBallType(string bType)
     {
-        this.ballType = bType;
-        GameObject.Find("SportName").GetComponent<TextMeshProUGUI>().SetText(ballType.ToUpper() + " EXERCISES");
-        Debug.Log(GameObject.Find("SportName").GetComponent<TextMeshProUGUI>().text);
+        addBallType(bType);
+        GameObject.Find("SportName").GetComponent<TextMeshProUGUI>().SetText(ballType.ToUpper() + " HAREKETLERİ");
+        initializeMovements(0);
     }
 
+    private void initializeMovements(int movementIndex) 
+    {
+        //videoPlayer = GetComponent<VideoPlayer>();
+        // videoPlayer.clip = videoC
 
+        switch (ballType) {
+            case "Futbol":
+                GameObject.Find("ExerciseTitle").GetComponent<TextMeshProUGUI>().SetText(footballMovements[movementIndex].ToUpper());
+                break;
+            case "Voleybol":
+                GameObject.Find("ExerciseTitle").GetComponent<TextMeshProUGUI>().SetText(volleyballMovements[movementIndex].ToUpper());
+                break;
+            case "Basketbol":
+                GameObject.Find("ExerciseTitle").GetComponent<TextMeshProUGUI>().SetText(basketballMovements[movementIndex].ToUpper());
+                break;
+        }
+
+        
+    }
+
+    private void videoFunction(VideoPlayer videoPlayer)
+    {
+        
+    }
+
+    private void addBallType(string bType) 
+    {
+        switch (bType)
+        {
+            case "Football_Cart":
+                this.ballType = "Futbol";
+                break;
+            case "Volleyball_Cart":
+                this.ballType = "Voleybol";
+                break;
+            case "Basketball_Cart":
+                this.ballType = "Basketbol";
+                break;
+        }
+    }
 
     //Volleyball Movements:
 
@@ -52,6 +109,25 @@ public class SelectionRoomMenu : MonoBehaviour
      * 
      * Between Legs Crossover Jump
      * Dribbling and Stop
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * ORTAK HAREKETLER
+     * 
+     * TEK BACAK TEMASLI AĞIRLIK AKTARMA
+     * YÜKSEKLİK ÜZERİNDEN YANA ÇİFT VACAK SIÇRAMALI BURPEE
+     * İKİ YANLI KARELAJ ZIPLAMA    
+     * TEK YANLI SOL AYAK KARELAJ ZIPLAMA
+     * TEK YANLI SAĞ AK KARELAJ ZIPLAMA
+     * DÖNMELİ YATAY SIÇRAMA
+     *  
+     *  
+     *  
+     *  
+     *  
      * 
      * 
      * 
