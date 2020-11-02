d3cd30d30079df964e9e36596a962e2190718bed
mertaksu11 <59760645+mertaksu11@users.noreply.github.com>
2020-11-02 22:15:53 +0300

    Common exercises interface added, Scene management applied.

---
+++ K-VR Unity Project/Assets/Scripts/Scene_Management_Scripts/SceneManagement_InitialScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class SceneManagement_InitialScene : MonoBehaviour
+{
+	
+	public GameObject definedButton;
+	public int sceneNumber;
+    // public UnityEvent OnClick = new UnityEvent();
+	
+    void Start()
+    {
+        definedButton = this.gameObject;
+    }
+	
+	// Update is called once per frame
+    void Update () {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+         
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Button Clicked");
+				SceneManager.LoadScene(sceneNumber);
+                // OnClick.Invoke();
+            }
+        }    
+    }
+}
