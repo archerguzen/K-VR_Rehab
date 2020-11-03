d24283e5865e70f1c897fc9b35f1548a76670e1d
mertaksu11 <59760645+mertaksu11@users.noreply.github.com>
2020-11-03 17:40:44 +0300

    Interface enhancements

--- K-VR Unity Project/Assets/Scripts/Scene_Management_Scripts/SceneManagement_InitialScene.cs
+++ K-VR Unity Project/Assets/Scripts/Scene_Management_Scripts/SceneManagement_InitialScene.cs
@@ -23,16 +23,26 @@ public class SceneManagement_InitialScene : MonoBehaviour
         RaycastHit Hit;
 		
 		Text t = definedButton.GetComponentInChildren(typeof(Text)) as Text;
+		Canvas can = definedButton.GetComponentInChildren(typeof(Canvas)) as Canvas;
 		if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
         {
-            Debug.Log("Hovering");
+            //Debug.Log("Hovering");
             // OnClick.Invoke();
 			// definedButton.transform.position += new Vector3(0,0.05f,0);
 			t.color = Color.red;
+			
+			if(sceneNumber != 1){
+				
+				definedButton.transform.Rotate(0.0f, 0.5f, 0.0f, Space.Self);
+				can.transform.Rotate(0.0f, -0.5f, 0.0f, Space.Self);
+			}
+			
         }
 		else{
+			
 			t.color = Color.black;
-		
+			definedButton.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+			can.transform.rotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
 		}
          
         if (Input.GetMouseButtonDown(0))
--- K-VR Unity Project/Assets/Scripts/Scene_Management_Scripts/SceneManagement_WorkoutScenes.cs
+++ K-VR Unity Project/Assets/Scripts/Scene_Management_Scripts/SceneManagement_WorkoutScenes.cs
@@ -20,12 +20,19 @@ public class SceneManagement_WorkoutScenes : MonoBehaviour
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit Hit;
 		
+		Text t = definedButton.GetComponentInChildren(typeof(Text)) as Text;
 		if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
         {
-            Debug.Log("Hovering");
+            // Debug.Log("Hovering");
             // OnClick.Invoke();
+			// definedButton.transform.position += new Vector3(0,0.05f,0);
+			t.color = Color.red;
         }
-         
+		else{
+			
+			t.color = Color.black;
+        }
+		
         if (Input.GetMouseButtonDown(0))
         {
             if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
